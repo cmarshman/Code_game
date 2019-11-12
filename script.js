8430ab2b12003f3dea49bd4312b30cdbf75f8872
Cory Marshman <corymarshman@Corys-MacBook-Air.local>
2019-11-11 19:45:32 -0500

    functional

--- script.js
+++ script.js
@@ -21,7 +21,7 @@ var questions = [
     }
 ]
 
-var startGame = document.getElementById("startQuiz");
+var startQuiz = document.getElementById("startQuiz");
 var begin = document.getElementById("begin");
 
 var questionCard = document.getElementById("questionCard");
@@ -30,6 +30,7 @@ var choiceA = document.getElementById("buttonA");
 var choiceB = document.getElementById("buttonB");
 var choiceC = document.getElementById("buttonC");
 var choiceD = document.getElementById("buttonD");
+var buttonEl = document.querySelector("#choice");
 
 questionCard.style.visibility = "hidden";
 
@@ -42,10 +43,55 @@ startQuiz.addEventListener("click", function (){
 });
 
 function promptQuestions() {
-    questionBox.style.visibility = "visibile";
-    titles.textContent = questions[i].title;
+    questionCard.style.visibility = "visible";
+    titles.textContent = questions[i].name;
     choiceA.textContent = questions[i].choices[0];
     choiceB.textContent = questions[i].choices[1];
     choiceC.textContent = questions[i].choices[2];
     choiceD.textContent = questions[i].choices[3];
 }
+
+var timeDisplay = document.getElementById("timer");
+
+function timeLeft() {
+      var timerInterval = setInterval(function () {
+          timer--;
+          timeDisplay.textContent = ("Time: " + timer);
+      }, 1000)
+      if (timer <= 0) {
+          clearInterval(timerInterval);
+      }
+}
+
+i = 0
+
+function nextQuestion() {
+    if ( i < questions.length) {
+        i++;
+        promptQuestions()
+    }
+}
+
+questionCard.addEventListener("click", function(event){
+    var element = event.target;
+  
+    if (element.matches("button") === true) {
+        var answer = element.textContent;
+        // console.log(answer);
+        // console.log("answer" + questions[i].answer);
+        if (answer == questions[i].answer) {
+            alert("Correct. Your a Coding Wizard");
+            timer = + 10;
+            nextQuestion ();
+        }
+        else {
+            alert("NOPE")
+            timer = - 10;
+            nextQuestion ();
+        } 
+    }
+    
+})
+
+
+
